e2e310a6d51f2b8923999f53f01e7bf4c2b19145
patilaarti15 <65292226+patilaarti15@users.noreply.github.com>
2020-09-04 06:20:27 +0530

    Update contact-us.component.ts

--- contactForm/contact-us.component.ts
+++ contactForm/contact-us.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactusComponent implements OnInit {
 
-  constructor() { }
+  public fbFormData=this.fb.group({
+    Fullname: ['',Validators.required],
+    Email: ['',Validators.compose([Validators.required, Validators.email])], 
+    Comment:['',Validators.required],
+    MobileNo:['',Validators.required] })
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
 
-}
+                    }
+  }
